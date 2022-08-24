a63ba585a8e5592188f77e76db3383f097474fb2
Andre Ostan <andre.ostan@gmail.com>
2022-08-24 17:15:58 -0300

    Usando template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,4 +4,8 @@ let job = "servidor público";
 let age = 34;
 let dreamplace = "London";
 
-console.log(name, email, job, age, dreamplace);
+// console.log(name, email, job, age, dreamplace);
+
+console.log(
+  `Olá, ${name}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${dreamplace}. Confirma pra gente alguns dados? A sua idade é ${age}, você é ${job} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+);
