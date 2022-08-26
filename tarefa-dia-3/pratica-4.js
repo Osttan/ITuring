53f2ee7dc98d0b1b7b10f3979582a6eb09404984
Andre Ostan <andre.ostan@gmail.com>
2022-08-26 12:48:25 -0300

    Verificando se o número é primo

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,16 @@
+let numero = 7;
+divisivel = 0;
+i = 1;
+
+while (i <= numero) {
+  if (numero % i == 0) {
+    divisivel++;
+  }
+  i++;
+}
+
+if (divisivel == 2) {
+  console.log(`Sim, o número ${numero} é primo!`);
+} else {
+  console.log(`Não, o número ${numero} não é primo!`);
+}
